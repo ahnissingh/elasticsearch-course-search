1ce73cde357d4dd35ac8370c6556a98de1f345a8
Ahnis <ahnisaneja@gmail.com>
2025-07-12 16:46:12 +0530

    initialize Spring Boot project with Docker Compose and Elasticsearch setup

---
+++ src/test/java/com/ahnis/searchapi/SearchapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahnis.searchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
